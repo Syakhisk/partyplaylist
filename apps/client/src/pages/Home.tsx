c671d147f80e85277cc55da612aa288641cf9351
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-04-02 23:53:13 +0700

    feat: add button component

--- apps/client/src/pages/Home.tsx
+++ apps/client/src/pages/Home.tsx
@@ -1,6 +1,29 @@
+import Button from "@/components/Button"
+import { ChevronUpIcon } from "@heroicons/react/24/outline"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex">
+      <Button variant="primary" disabled>Hello</Button>
+      <Button variant="warning">Hello</Button>
+      <Button variant="secondary">Hello</Button>
+
+      <Button variant="primary" outlined>
+        Hello
+      </Button>
+      <Button variant="warning" outlined>
+        Hello
+      </Button>
+      <Button variant="secondary" outlined>
+        Hello
+      </Button>
+
+
+      <Button Icon={ChevronUpIcon} variant="primary" />
+      <Button Icon={ChevronUpIcon} variant="warning" />
+      <Button Icon={ChevronUpIcon} variant="secondary" />
+
+    </div>
   )
 }
 
