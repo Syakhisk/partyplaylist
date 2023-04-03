5018d32e42796990a74406e40009d801469cbbc6
xyedo <hafidmahdi23@gmail.com>
2023-04-04 06:02:15 +0700

    feat:authorization using firebase-auth

--- apps/api/src/files/file.interface.ts
+++
@@ -1,5 +0,0 @@
-import { FileEntity } from 'src/files/entities/files.entity';
-
-export interface IFileRepository {
-  findFileById(id: string): Promise<FileEntity>;
-}
--- apps/api/src/security/security.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface security {
-  hashPassword(rawPassword: string): Promise<string>;
-  comparePassword(hash: string, password: string): Promise<void>;
-}
