7c05b59404947b3a413bedfd676a3c1be1ebc205
xyedo <hafidmahdi23@gmail.com>
2023-04-08 21:12:37 +0700

    feat:init front-end socket.io

--- apps/client/src/constants.ts
+++ apps/client/src/constants.ts
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client"
-export const API_BASE_URL: string =
-  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000/api/v1"
+const BASE_URL = "http://localhost:3000"
+export const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL ?? `${BASE_URL}/api/v1`
 
-export const socket: ReturnType<typeof io> = io(API_BASE_URL, { autoConnect: false })
+export const socket: ReturnType<typeof io> = io(BASE_URL, {
+  autoConnect: false,
+})
