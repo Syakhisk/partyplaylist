6d2b2c64df06d46ac5d1f5fa0dba450782b68885
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-04-15 00:15:27 +0700

    fix: allow commonjs and add eslint on shared pkg

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,7 +5,10 @@ import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint({ useEslintrc: true })],
+  plugins: [react(), eslint({ useEslintrc: true, exclude: ["**/node_modules/**", "**/dist/**"] })],
+  optimizeDeps: {
+    include: ['schema']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
